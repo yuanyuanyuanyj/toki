e27b6f03b3b70bdf1608dd960b85a8f29d0c0341
Administrator <Administrator@yuzongqi>
2020-06-02 09:29:15 +0800

    aa

---
+++ src/test/java/zh/toki/TokiApplicationTests.java
@@ -0,0 +1,13 @@
+package zh.toki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
